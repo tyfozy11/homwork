a9c82ccf879929885852cf65be94ec76f250c983
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-07-07 20:56:48 +0300

    New works

--- Lesen 3.py
+++ Lesen 3.py
@@ -1,4 +1,4 @@
-#lesson 3
+# lesson 3
 my_str = 'abc abc abv sdc'
 
 result = my_str.split("ab")
@@ -12,3 +12,47 @@ print(my_list)
 my_list.remove(2)
 print(my_list)
 print("df" in my_list)
+my_list[2]="2323"
+print(my_list)
+
+# loop
+a = 0
+b = 10
+while a < b:
+    print(f'a is {a} b is {b}')
+    a+=1 # a = a + 1
+a = 0
+b = 10
+
+#while True:
+ #   print(f'a is {a} b is {b}')
+ #   a+=1 # a = a + 1
+ #   if a % 2 == 0:
+ #       continue
+ #   if a >= b:
+ #       break
+
+#lst = [1, 22, 2, 'T', 44, 52]
+#idx = 0
+#while idx < len(lst):
+ #   print(lst[idx])
+
+ #   idx += 1
+
+#while True:
+#    user_input = input("Enter the number")
+ #   if user_input.isdigit():
+#       print("Done")
+#    else:
+#        print('It\' s not number')
+
+#lst = [1, 22, 2, 'T', 44, 52]
+#for i in lst:
+ #   print('--->', i)
+
+#lst2 =[ [2, 3, 4,[5,6, 7,],[8, 9, 10] ]
+#for value in lst2:
+ #   print('-->', value)
+
+  #  for sub_val in value:
+  #      print('--->', sub_val)
