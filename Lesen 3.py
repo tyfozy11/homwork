3285be66517fa930a187a110cdcfbe092c8bb461
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-07-07 19:45:37 +0300

    New works

---
+++ Lesen 3.py
@@ -0,0 +1,14 @@
+#lesson 3
+my_str = 'abc abc abv sdc'
+
+result = my_str.split("ab")
+print(my_str)
+print(result)
+print(type(result))
+my_list = [1, 2, "df", True, [23, 44, 52]]
+print(my_list)
+my_list.append(my_str)
+print(my_list)
+my_list.remove(2)
+print(my_list)
+print("df" in my_list)
