6f68b7004b23010bdc3f900030ac6f9dc7880388
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-09-03 17:37:58 +0300

    new date

--- homework 15.py
+++ homework 15.py
@@ -2,24 +2,18 @@ import requests
 import datetime
 
 url_nbu = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-result = requests.request('GET', url_nbu)
-
-
-with open('example.txt', 'a', encoding='utf-8') as file:
-    file.write(str(datetime.datetime.now()) + '\n\n')
-    position = 1
-    for currency in result.json():
-        file.write(f'{position}. {currency["txt"]} - {currency["rate"]}' + '\n')
-        position += 1
-
 
 try:
-    result = requests.request('GET', url)
+    data_from_url_nbu = requests.request('GET', url_nbu)
 except Exception as e:
     print(e)
 else:
-    if 300 > result.status_code >= 200:
-        if content := result.headers.get('Content-Type'):
-            if content == 'image/png':
-                with open('logo.png', 'wb') as file:
-                    file.write(result.content)
+    if 300 > data_from_url_nbu.status_code >= 200:
+        if content := data_from_url_nbu.headers.get('Content-Type'):
+            if content == 'application/json; charset=utf-8':
+                with open('example.txt', 'a', encoding='utf-8') as file:
+                    file.write(str(datetime.datetime.now()) + '\n\n')
+                    position = 1
+                    for currency in data_from_url_nbu.json():
+                        file.write(f'{position}. {currency["txt"]} - {currency["rate"]}' + '\n')
+                        position += 1
